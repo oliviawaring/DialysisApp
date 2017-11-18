f9aa3a6c3804845143f5df1f78684fe1a0d1a544
arafasse <olivia.waring@gmail.com>
2017-11-18 17:07:24 -0500

    to debug Amelia's buttons

--- dialysisBuddy/buttons.h
+++ dialysisBuddy/buttons.h
@@ -10,7 +10,7 @@ int readButtons(int &pageNum)
  //Serial.print("in buttons");
  //int adc_key_in  = 0;
  int adc_key_in = analogRead(A0);      // read the value from the sensor 
- //Serial.println(adc_key_in);
+ Serial.println(adc_key_in);
  // my buttons when read are centered at these valies: 140, 283, 425, 568, 710
  // we add approx 50 to those values and check to see if we are close
  if ((adc_key_in > 1000) || (adc_key_in < 90)) return btnNONE; // We make this the 1st option for speed reasons since it will be the most likely result
