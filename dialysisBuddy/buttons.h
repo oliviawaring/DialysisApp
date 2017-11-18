9952b0ef183299e76d5b88246566e0584226b21a
arafasse <olivia.waring@gmail.com>
2017-11-18 17:16:56 -0500

    Changing the voltage values

--- dialysisBuddy/buttons.h
+++ dialysisBuddy/buttons.h
@@ -13,33 +13,33 @@ int readButtons(int &pageNum)
  Serial.println(adc_key_in);
  // my buttons when read are centered at these valies: 140, 283, 425, 568, 710
  // we add approx 50 to those values and check to see if we are close
- if ((adc_key_in > 1000) || (adc_key_in < 90)) return btnNONE; // We make this the 1st option for speed reasons since it will be the most likely result
+ if ((adc_key_in > 1200) || (adc_key_in < 151)) return btnNONE; // We make this the 1st option for speed reasons since it will be the most likely result
  // For V1.0 comment the other threshold and use the one below:
 
- if (adc_key_in < 190)   
+ if (adc_key_in < 251)   
  {
     Serial.print("help");
     getHelp(pageNum);
     return btnHELP;   
  }
- if (adc_key_in < 333)  
+ if (adc_key_in < 456)  
  {
     Serial.print("back");
     return btnBACK; 
  }
- if (adc_key_in < 475)
+ if (adc_key_in < 661)
  {
    Serial.print("go");
    return btnGO;
  }
- if (adc_key_in < 618)
+ if (adc_key_in < 868)
  {
    Serial.print("down");
    goNext(pageNum);
    showPage(pageNum, NULL);
    return btnDOWN; 
  }
- if (adc_key_in < 760)
+ if (adc_key_in < 1073)
  {
    Serial.print("up");
    goBack(pageNum);
