5f326a8dad3056c5f54a79515b2966dce5779d5a
arafasse <olivia.waring@gmail.com>
2017-11-20 21:41:24 -0500

    Revert "Adding VERY PRELIMINARY data storage capabilities"
    This reverts commit 29ea1160323b3e3172c77ad1d5b254065e43c878.

--- dialysisBuddy/graphing.h
+++
@@ -1,2 +0,0 @@
-// a matlab interface might not be our best option, because then we'd either need to store the program on our SD card (probably not remotely possible or advisable) or interface with wifi. There's no point having people run matlab on their own data, cuz they're not going to have access to that software in any way, shape or form... Tis a pity though, cuz Matlab is SO. EASY. and perfect for this sort of graphing application. But it won't work here. :( 
-// Ooooh. Would R work????
