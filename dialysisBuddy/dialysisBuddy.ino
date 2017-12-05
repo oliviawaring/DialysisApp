28f455e2e24d5497c3d39526890eda0f2cb1be47
arafasse <olivia.waring@gmail.com>
2017-12-04 19:03:16 -0500

    Switch to Mega + Adafruit tft; upload bitmaps

--- dialysisBuddy/dialysisBuddy.ino
+++ dialysisBuddy/dialysisBuddy.ino
@@ -7,10 +7,10 @@
 #define RESETLINE     30
 #define DisplaySerial Serial1
 
-#include "Picaso_Serial_4DLib.h"
-#include "Picaso_Const4D.h"
 #include <SPI.h>
-#include <SD.h>
+#include <SD.h>       // this is needed for display
+#include <Adafruit_GFX.h>    // Core graphics library
+#include <Adafruit_ILI9340.h>
 #include "Keypad.h"
 #include "Math.h"
 #include "page.h"
@@ -21,8 +21,15 @@
 #include "keyboard.h"
 #include "pageOperations.h"
 #include "buttons.h"
+#include "bitmaps.h"
 
-Picaso_Serial_4DLib Display(&DisplaySerial);
+#define TFT_RST 8
+#define TFT_DC 9
+#define TFT_CS 10
+#define SD_CS 4
+
+Adafruit_ILI9340 screen = Adafruit_ILI9340(TFT_CS, TFT_DC, TFT_RST);
+//TouchScreen ts = TouchScreen(XP, YP, XM, YM, 300);
 //Keypad kpd;
 
 word x, y ; // Touchscreen coordinates (keeping this in case we need to resurrect the touchscreen later)
@@ -34,32 +41,51 @@ Session currentSession = {0, 0, 0, 0, {0, 0}, {0, 0}, {0, 0}, {0, 0}, 0, 0, 0, 0
 // Prepare the 4duino for program deployment
 void setup()
 {
-  pinMode(RESETLINE, OUTPUT);       // Display reset pin
-  digitalWrite(RESETLINE, 1);       // Reset Display, using shield
-  delay(100);                       // Wait for it to be recognised
-  digitalWrite(RESETLINE, 0);       // Release Display Reset, using shield
-  delay(4000) ;                     // Give display time to startup
+  Serial.begin(9600);
+
+  pinMode(SD_CS, OUTPUT);
+  digitalWrite(SD_CS, HIGH);
+  
+  screen.begin();
+  screen.fillScreen(ILI9340_BLUE);
+  
+
+  Serial.print("Initializing SD card...");
+  if (!SD.begin(SD_CS)) {
+    Serial.println("failed!");
+    return;
+  }
+  Serial.println("OK!");
+
+  bmpDraw("main.bmp", 0, 0);
+  
+ // pinMode(RESETLINE, OUTPUT);       // Display reset pin
+ // digitalWrite(RESETLINE, 1);       // Reset Display, using shield
+ // delay(100);                       // Wait for it to be recognised
+ // digitalWrite(RESETLINE, 0);       // Release Display Reset, using shield
+ // delay(4000) ;                     // Give display time to startup
+
+ // screen.begin();
+ // yield();
 
   // Start display as Serial lines should have 'stabilised'
-  DisplaySerial.begin(200000) ;     // Hardware serial to Display, same as SPE on display is set to
-  Display.TimeLimit4D = 5000 ;      // 5 second timeout on all commands
+ // DisplaySerial.begin(200000) ;     // Hardware serial to Display, same as SPE on display is set to
+ // Display.TimeLimit4D = 5000 ;      // 5 second timeout on all commands
   
-  Display.gfx_ScreenMode(PORTRAIT); // change manually if orientation changes
-  pinMode(13, OUTPUT);
-  Display.touch_Set(TOUCH_ENABLE); // enable the touch screen
+  //Display.gfx_ScreenMode(PORTRAIT); // change manually if orientation changes
+  // pinMode(13, OUTPUT);
+  //Display.touch_Set(TOUCH_ENABLE); // enable the touch screen
   // makePages(); We might use this function later... 
 
   // Initialize SD card
-  while (!Display.media_Init())
+//  while (!Display.media_Init())
   {
-     Display.gfx_Cls();
+    //Display.gfx_Cls();
      delay(300);
-     Display.putstr("Please insert SD card");
+     //Display.putstr("Please insert SD card");
      delay(300);
   }
-  
   goHome();
-
 } // end Setup **do not alter, remove or duplicate this line**
 
 // The main program loop
@@ -68,6 +94,7 @@ void loop()
    // Launch opening menu behavior
    if (inHomePage)
    {
+//      bmpDraw("main.bmp", 0, 0);
       Serial.print("home!\n");
       Serial.print(sectionNum);
       Serial.print(pageNum);
